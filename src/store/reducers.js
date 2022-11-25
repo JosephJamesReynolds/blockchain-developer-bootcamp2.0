f11666af12d4a8572d0f353997af826885416d97
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2022-11-24 16:01:36 -0800

    29. Cancel Orders

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -120,6 +120,49 @@ case 'ALL_ORDERS_LOADED':
     }
   } 
 
+// -------------------------------
+// CANCELLING ORDERS
+
+case 'ORDER_CANCEL_REQUEST':
+  return {
+    ...state,
+    transaction: {
+      transactionType: 'Cancel',
+      isPending: true,
+      isSuccessful: false
+    }
+  }
+
+case 'ORDER_CANCEL_SUCCESS':
+  return {
+    ...state,
+    transaction: {
+    transactionType: 'Cancel',
+    isPending: false,
+    isSuccessful: true
+  },
+  cancelledOrders: {
+    ...state.cancelledOrders,
+    data: [
+    ...state.cancelledOrders.data,
+    action.order
+    ]
+  },
+  events: [action.event, ...state.events]
+}
+
+case 'ORDER_CANCEL_FAIL':
+  return { 
+    ...state,
+    transaction: {
+      transactionType: 'Cancel',
+      isPending: false,
+      isSuccessful: false,
+      isError: true
+    }
+  }  
+
+
 //.....................-------------------------------
 //...................... BALANCE CASES
     case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
